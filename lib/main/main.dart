193e8cca531c7002bb3f821171420cae08a7667d
Francis Chua Gregorio <fcgregorio@gmail.com>
2020-09-06 22:19:01 +0800

    New
    Added MaterialApp as root widget.
    Added MainScreen.

---
+++ lib/main/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:talipapa/main/home.dart';
+import 'package:talipapa/main/profile.dart';
+
+class MainScreen extends StatefulWidget {
+
+  MainScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (BuildContext context) {
+          switch (_selectedIndex) {
+            case 0:
+              return HomeScreen();
+            default:
+              throw Error();
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
